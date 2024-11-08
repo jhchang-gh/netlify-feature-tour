62dd0fbc169f7e8612da9b3a47004821809d091c
Jeff Chang <tsavij@gmail.com>
2024-11-08 16:35:11 -0500

    tailwind and some edits, component step

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://feature-tour.netlify.app',
-  integrations: [
-    preact(),
-    sitemap({
-      canonicalURL: 'https://feature-tour.netlify.app'
-    })
-  ],
+  integrations: [preact(), sitemap({
+    canonicalURL: 'https://feature-tour.netlify.app'
+  }), tailwind()]
 });
